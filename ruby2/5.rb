565718afd656fcc2c803cfbbe24ea4c3839564ea
qagushaq <qwerty24164444@gmail.com>
2020-12-03 19:21:29 +0300

    add_mm_dd_yyyy_format

--- ruby2/5.rb
+++ ruby2/5.rb
@@ -1,9 +1,10 @@
-puts 'Введите число:'
-number = gets.chomp.to_i
-puts 'Введите месяц:'
-month = gets.chomp.to_i
-puts 'Введите год:'
-year = gets.chomp.to_i
+puts ("Введите дату в формате дд.мм.гггг")
+mm_dd_yyyy = gets().chomp.split(".")
+mm_dd_yyyy.map! {|item| item = item.to_i }
+
+number = mm_dd_yyyy[0]
+month = mm_dd_yyyy[1]
+year = mm_dd_yyyy[2]
 
 days_in_a_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0)
